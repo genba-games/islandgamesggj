d02e11126b176476bbda8eabf5087bdb50793bd7
Diego Díaz <berith.py@gmail.com>
2017-01-22 14:23:35 -0300

    Server ip input in titlescreen.

--- src/sources/IslandWaves/States/menuState.js
+++ src/sources/IslandWaves/States/menuState.js
@@ -1,19 +1,21 @@
 // The menu state handles the server connection
+
 var menuState = function () { };
 menuState.prototype =
     {
         preload: function () {
             // Load assets
             game.load.image('title_screen', 'src/graphics/title_screen.png');
-            game.load.spritesheet('logo', 'src/graphics/logoSprite.png',370,388);
+            game.load.spritesheet('logo', 'src/graphics/logoSprite.png', 370, 388);
             game.load.audio('beach_sound', 'src/audio/beach.wav')
 
         },
 
         create: function () {
+            game.add.plugin(PhaserInput.Plugin);
             // Set background
             game.add.sprite(0, 0, 'title_screen');
-            
+
             music = game.add.audio('beach_sound');
             music.loop = true;
             music.play();
@@ -25,9 +27,19 @@ menuState.prototype =
             // FIXME Placeholder
             logo = game.add.button(175, 25, 'logo', this.connect, this);
             var animate = logo.animations.add('animate');
-            logo.animations.play('animate',7,true);
-        },
+            logo.animations.play('animate', 7, true);
 
+            ipInput = game.add.inputField(600 / 2 - 20, 450, {
+                fillAlpha: 0,
+                width: 200,
+                font: '18px JinxedWizards',
+                placeHolder: '10.10.5.69:3000',
+                placeHolderColor: '#000000',
+            });
+        },
+        update: function(){
+
+        },
         /**
          * Tries to connect to a server and creates the game.
          */
@@ -39,6 +51,6 @@ menuState.prototype =
 
             // TODO Setup input to define host
             // socket = open_connection('192.168.0.207:3000', start_game);
-            socket = open_connection('localhost:3000', start_game);
+            socket = open_connection(ipInput.text._text, start_game);
         },
     };
